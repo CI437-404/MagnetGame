de96639284147e3cb1a55751c8058d1e02c4f939
Nick Olenz <nickolenz@comcast.net>
2017-04-25 16:24:05 -0500

    Conveyor belts.

---
+++ Assets/Scripts/Triggerables/Conveyor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Pushes things on the x axis of this object by using evil collider magic.
+// Negative speed values to go the other way.
+public class Conveyor : Triggerable
+{
+	bool active = false;
+	Rigidbody2D rb;
+
+	public Vector2 direction;
+	public float speed = 2.0f;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+	{
+		direction = transform.TransformDirection(new Vector2(1f, 0f));
+		if (active)
+		{
+			rb.position -= direction * speed * Time.deltaTime;
+			rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+		}
+	}
+
+	public override void Trigger()
+	{
+		active = true;
+	}
+
+	public override void UnTrigger()
+	{
+		active = false;
+	}
+}
